ebee015d02171638dfd479194dfa9ee3c7460c6e
Julia Capanema <julia.capanema@univille.br>
2024-06-22 00:10:35 -0300

    FIX - Ajustando componente Footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -5,12 +5,9 @@ const Footer = () => {
     return (
         <footer className={classNames('position-relative bottom-0 text-white w-100 mt-auto', styles.footer)}>
             <div className="d-grid">
-                <div className="row">
-                    <div className="col-lg-6 text-lg-start mt-1">
+                <div className="row m-0">
+                    <div className="text-lg-center mt-3">
                         <p>&copy; {new Date().getFullYear()} Germoneys. Todos os direitos reservados.</p>
-                    </div>
-                    <div className="col-lg-6 text-lg-end mt-1">
-                        <p>Entre em contato: projeto.germoneys@gmail.com</p>
                     </div>
                 </div>
             </div>
