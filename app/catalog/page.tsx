d1debeeee7cbeb9e8156f397c09961384b6dab7c
Julia Capanema <julia.capanema@univille.br>
2024-06-23 15:24:22 -0300

    FEAT - Criando estrutura catalogo

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,10 +1,38 @@
 import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
+import InputValue from "@/components/inputValue";
+import FilterButton from "../../components/filterButton";
+import styles from "./page.module.scss"
+import classNames from "classnames";
+import {Product} from "@/types/products";
+import api from "@/api/api";
 
-export default function Page(){
+async function getProducts(): Promise<Product[]> {
+    try {
+        const response = await api.get('/products')
+        return response.data;
+    } catch (error) {
+        console.error('Failed to fetch transactions', error);
+        return [];
+    }
+}
+
+export default async function Page() {
+    const products: Product[] = await getProducts();
+
+    console.log(products)
     return (
         <>
             <NavBar/>
+            <div className={"p-5"}>
+                <div className={classNames("rounded", styles.pageBox)}>
+                    <h1 className={styles.text}>Cumpriu Tarefas, Ganhou Germoneys, Escolheu Prêmios!</h1>
+                    <div className={"p-5 d-flex justify-content-center align-items-start"}>
+                        <InputValue/>
+                        <FilterButton/>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </>
     )
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 const NavBar = () => {
     return (
         <>
-            <nav className={classNames('navbar position-fixed w-100', styles.navBar)}>
+            <nav className={classNames('navbar position-relative top-0 w-100', styles.navBar)}>
                 <div className={classNames('container-sm justify-content-center', styles.navbarText)}>
                     <img src="/logoicon.ico" alt="Logo" width="40" height="40"
                          className="d-inline-blockalign-text-bottom"/>
