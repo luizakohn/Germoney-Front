fab29f07a66be81d701d62fb091c1cc002501e0e
Julia Capanema <julia.capanema@univille.br>
2024-06-23 15:22:32 -0300

    FEAT - Criando componente inputValue

---
+++ components/inputValue/index.tsx
@@ -0,0 +1,3 @@
+import InputValue from "@/components/inputValue/inputValue";
+
+export default InputValue
---
+++ components/inputValue/inputValue.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { Input, InputGroup, InputGroupText } from "reactstrap";
+import styles from "./inputValue.module.scss";
+import { useState } from "react";
+
+const InputValue = () => {
+    const [value, setValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
+
+    const handleChange = (e: { target: { value: any; }; }) => {
+        const newValue = e.target.value;
+        setValue(newValue);
+        validateValue(newValue);
+    };
+
+    const validateValue = (value: string) => {
+        const parsedValue = parseFloat(value);
+        if (!isNaN(parsedValue) && parsedValue > 0) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+    };
+
+    return (
+        <div className={styles.inputContainer}>
+            <InputGroup className={styles.inputValue}>
+                <InputGroupText>
+                    $
+                </InputGroupText>
+                <Input
+                    type="number"
+                    value={value}
+                    placeholder="Insira o valor de Germoneys"
+                    onChange={handleChange}
+                    className={isValid ? '' : styles.invalid}
+                />
+            </InputGroup>
+            {!isValid && <div className={styles.error}>Por favor, insira um valor válido.</div>}
+        </div>
+    );
+};
+
+export default InputValue;
